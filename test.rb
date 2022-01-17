7546fb0a516e370e060a18660c03d803c5b58923
katsumikosen <j1801@matsue-ct.ac.jp>
2022-01-17 17:37:56 +0900

    test.rb

--- test.rb
+++ test.rb
@@ -1,1 +1,12 @@
+for i in 1..100 do
+  if i.modulo(3)==0&&i.modulo(5)==0 then
+    print ("FizzBuzz\n")
+    elsif i.modulo(3)==0 then
+      print ("Fizz\n")
+    elsif i.modulo(5)==0 then
+      print("Buzz\n")
+    else
+      print ("i\n")
+    end
+end
 
